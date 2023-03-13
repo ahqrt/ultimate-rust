16d823bbcbad051410f50b03272823e19f3bf9ac
yangliye <1217223113@qq.com>
2023-03-13 14:59:52 +0800

    feat: add guess cmp to scret_num

--- guess_num/src/main.rs
+++ guess_num/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
 
@@ -9,5 +10,13 @@ fn main() {
     println!("Please input your guess.");
     let mut guess = String::new();
     io::stdin().read_line(& mut  guess).expect("Failed to read line");
+    let guess : u32 = guess.trim().parse().expect("Please type a number");
     println!("You guess, {guess}");
+    match guess.cmp(&secret_num) {
+        Ordering::Less => println!("Too small"),
+        Ordering::Equal => println!("You win"),
+        Ordering::Greater => println!("Too big")
+
+
+    }
 }
