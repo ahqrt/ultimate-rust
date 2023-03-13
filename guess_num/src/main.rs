e02c13d89dad9daf51517a9af15c01119b454f16
yangliye <1217223113@qq.com>
2023-03-13 14:42:54 +0800

    feat: the basic usage of rand

--- guess_num/src/main.rs
+++ guess_num/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use rand::Rng;
+
 fn main() {
-    println!("Hello, world!");
+
+    println!("Guess the number!!");
+    let secret_num = rand::thread_rng().gen_range(1..=100);
+    println!("The secret num is :{secret_num}");
+    println!("Please input your guess.");
+    let mut guess = String::new();
+    io::stdin().read_line(& mut  guess).expect("Failed to read line");
+    println!("You guess, {guess}");
 }
