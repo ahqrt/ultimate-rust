25c1dfe3445193f2bed2154b185c801c846e8abd
yangliye <1217223113@qq.com>
2023-03-14 18:13:30 +0800

    chore: add array test

---
+++ array/src/main.rs
@@ -0,0 +1,15 @@
+use std::io::{self, Read};
+
+fn main() {
+    let a = [1, 2, 3, 4, 5];
+    println!("Please enter an array index.");
+    let mut index = String::new();
+
+    io::stdin().read_line(&mut index).unwrap();
+
+    let index: usize = index.trim().parse().unwrap();
+
+    let element = a[index];
+
+    println!("The value of the element at index {index} is {element}")
+}
