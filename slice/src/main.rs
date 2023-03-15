974bb411458001815e4daa4aca002a49b14cbead
yangliye <1217223113@qq.com>
2023-03-15 16:15:47 +0800

    chore: change param type

--- slice/src/main.rs
+++ slice/src/main.rs
@@ -17,7 +17,8 @@ fn main() {
     println!("{hello}, {word}")
 }
 
-fn first_word(s: &String) -> &str {
+// you can use &str as the param type both support &str and &String
+fn first_word(s: &str) -> &str {
     let bytes = s.as_bytes();
     for (i, &item) in bytes.iter().enumerate() {
         if item == b' ' {
