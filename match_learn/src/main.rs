68a7ba949d28a0ca1ede51ee137e6bf9c9c31ddc
ahqrt <1217223113@qq.com>
2023-03-17 23:41:06 +0800

    feat: add enum example

---
+++ match_learn/src/main.rs
@@ -0,0 +1,21 @@
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter,
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => 1,
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter => 25,
+    }
+}
+
+fn main() {
+    let a = Coin::Dime;
+    let res = value_in_cents(a);
+    println!("res, {}", res)
+}
