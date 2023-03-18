70aeb98f60329f78091abd00f61de706bf7ad833
ahqrt <1217223113@qq.com>
2023-03-18 15:51:51 +0800

    feat: split mod in to file

--- backyard/src/lib.rs
+++ backyard/src/lib.rs
@@ -1,28 +1,7 @@
-pub mod front_of_house {
-    pub mod hosting {
-        pub fn add_to_waitlist() {}
+mod front_of_house;
 
-        fn set_at_table() {}
-    }
+pub use crate::front_of_house::hosting;
 
-    mod serving {
-        fn take_order() {}
-        fn serve_order() {}
-        fn take_payment() {}
-    }
-}
-
-// using use to create a shorter name
-
-mod customer {
-    use crate::front_of_house::hosting;
-
-    pub fn eat_at_restaurant() {
-        // absolute path
-        // crate::front_of_house::hosting::add_to_waitlist();
-        hosting::add_to_waitlist();
-
-        // relative path
-        hosting::add_to_waitlist()
-    }
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
 }
