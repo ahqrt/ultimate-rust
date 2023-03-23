a0b2833db03932d7c09522674490db1d2fc89ffc
yangliye <1217223113@qq.com>
2023-03-23 16:35:40 +0800

    add another useage of generic

--- enum_learn/src/main.rs
+++ enum_learn/src/main.rs
@@ -37,21 +37,30 @@ fn largest<T: std::cmp::PartialOrd>(list: &[T]) -> &T {
 
 // generic struct
 
-struct Point<T> {
+struct Point<T, U> {
     x: T,
-    y: T,
+    y: U,
 }
 
 // impl method definitions
-impl<T> Point<T> {
-    fn x(&self) -> &T {
-        &self.x
-    }
-}
+// impl<T> Point<T> {
+//     fn x(&self) -> &T {
+//         &self.x
+//     }
+// }
 
-impl Point<f32> {
-    fn distance_from_origin(&self) -> f32 {
-        (self.x.powi(2) + self.y.powi(2)).sqrt()
+// impl Point<f32> {
+//     fn distance_from_origin(&self) -> f32 {
+//         (self.x.powi(2) + self.y.powi(2)).sqrt()
+//     }
+// }
+
+impl<X1, Y1> Point<X1, Y1> {
+    fn mixup<X2, Y2>(self, other: Point<X2, Y2>) -> Point<X1, Y2> {
+        Point {
+            x: self.x,
+            y: other.y,
+        }
     }
 }
 
@@ -64,9 +73,18 @@ fn main() {
     // let result = largest(&char_list);
     // println!("The largest char is {}", result);
 
-    let integer = Point { x: 5, y: 10 };
-    let float: Point<f32> = Point { x: 12.0, y: 11.0 };
+    // let integer = Point { x: 5, y: 10 };
+    // let float: Point<f32> = Point { x: 12.0, y: 11.0 };
 
-    println!("{}", integer.x());
-    println!("{}", float.distance_from_origin());
+    // println!("{}", integer.x());
+    // println!("{}", float.distance_from_origin());
+
+    let p1 = Point { x: 5, y: 10.5 };
+    let p2 = Point {
+        x: "Hello world",
+        y: 'c',
+    };
+
+    let p3 = p1.mixup(p2);
+    println!("p3.x {}, p3.y {}", p3.x, p3.y);
 }
