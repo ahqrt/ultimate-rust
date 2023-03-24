faa5d78788b7a795e078a3c58cf1535ce12f5420
yangliye <1217223113@qq.com>
2023-03-24 11:43:17 +0800

    feat: finished basic fn of minigrep

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,3 +1,13 @@
+use std::{env, fs};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    let query = &args[1];
+    let file_path = &args[2];
+
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+
+    println!("Searching for {}", query);
+    println!("In file {}", file_path);
+    println!("With text:\n {}", contents);
 }
