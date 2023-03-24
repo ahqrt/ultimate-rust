030409e586a9c8d1daa8b6f1f6cf31eccd32fe1b
yangliye <1217223113@qq.com>
2023-03-24 18:04:56 +0800

    feat: writing error message to standard error

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -6,12 +6,12 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = Config::build(&args).unwrap_or_else(|err| {
-        println!("problem parsing arguments: {err}");
+        eprintln!("problem parsing arguments: {err}");
         process::exit(1);
     });
 
     if let Err(e) = run(config) {
-        println!("Application errror: {e}");
+        eprintln!("Application errror: {e}");
         process::exit(1);
     }
 }
